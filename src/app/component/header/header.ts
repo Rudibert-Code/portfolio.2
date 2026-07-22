fbdf1fc6dba305c6fa50d0c7629f06f99fba92ad
Bjoern <onoofelogmail.com>
2026-07-22 19:21:31 +0200

    - wrote function markElement() for user feedback when selecting shortcut in nav-bar

--- src/app/component/header/header.ts
+++ src/app/component/header/header.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  quickLink(link:string){
+    window.open(link);
+  }
+}
--- src/app/component/nav-bar/nav-bar.ts
+++ src/app/component/nav-bar/nav-bar.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
-export class NavBar {}
+export class NavBar {
+  markElement(id:string){
+    let targetElement = document.getElementById(id) as HTMLParagraphElement;
+    this.clearAllElements();
+    targetElement.classList.toggle('marked');
+  }
+  
+  clearAllElements(){
+    let e1 = document.getElementById('why-me') as HTMLParagraphElement;
+    let e2 = document.getElementById('skills') as HTMLParagraphElement;
+    let e3 = document.getElementById('projects') as HTMLParagraphElement;
+    let e4 = document.getElementById('contacts') as HTMLParagraphElement;
+    e1.classList.remove('marked');
+    e2.classList.remove('marked');
+    e3.classList.remove('marked');
+    e4.classList.remove('marked');
+  }
+}
