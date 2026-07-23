278974b77112154baf7e08c1523119a1d4f1c0bb
Bjoern <onoofelogmail.com>
2026-07-23 09:33:48 +0200

    added DE - EN toggle

--- src/app/component/nav-bar/nav-bar.ts
+++ src/app/component/nav-bar/nav-bar.ts
@@ -7,13 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.scss',
 })
 export class NavBar {
-  markElement(id:string){
+  // markiert Navigationselement ...
+  markElement(id:string,type:string){
     let targetElement = document.getElementById(id) as HTMLParagraphElement;
-    this.clearAllElements();
+    switch (type) {
+      case 'nav':
+        this.clearNavigation();
+        break;
+      case 'lan':
+        this.clearLanguage();
+        break;
+      default:
+        break;
+    }
     targetElement.classList.toggle('marked');
   }
   
-  clearAllElements(){
+  // löscht Markierung auf Navigationselement ...
+  clearNavigation(){
     let e1 = document.getElementById('why-me') as HTMLParagraphElement;
     let e2 = document.getElementById('skills') as HTMLParagraphElement;
     let e3 = document.getElementById('projects') as HTMLParagraphElement;
@@ -22,5 +33,12 @@ export class NavBar {
     e2.classList.remove('marked');
     e3.classList.remove('marked');
     e4.classList.remove('marked');
+  }
+
+  clearLanguage(){
+    let l1 = document.getElementById('DE') as HTMLParagraphElement;
+    let l2 = document.getElementById('EN') as HTMLParagraphElement;
+    l1.classList.remove('marked');
+    l2.classList.remove('marked');
   }
 }
