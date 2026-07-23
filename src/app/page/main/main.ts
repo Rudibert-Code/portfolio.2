486f9942d39074502599ec7ecdd57da3dc09400f
Bjoern <onoofelogmail.com>
2026-07-23 11:13:09 +0200

    - created "my-skills" section - fixed "arrow" buttons - set href links in buttons

--- src/app/page/main/main.ts
+++ src/app/page/main/main.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '../../component/header/header';
 import { NavBar } from '../../component/nav-bar/nav-bar';
 import { WhyMe } from '../../component/why-me/why-me';
+import { MySkills } from '../../component/my-skills/my-skills';
 
 @Component({
   selector: 'app-main',
-  imports: [Header, NavBar, WhyMe],
+  imports: [Header, NavBar, WhyMe, MySkills],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
