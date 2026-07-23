111c484f77357be95889e7801dbc284b222805ea
Bjoern <onoofelogmail.com>
2026-07-23 11:30:40 +0200

    inported assets

--- src/app/component/my-skills/my-skills.ts
+++ src/app/component/my-skills/my-skills.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Skills{
+  img:string,
+  name:string,
+}
+
 @Component({
   selector: 'app-my-skills',
   imports: [],
   templateUrl: './my-skills.html',
   styleUrl: './my-skills.scss',
 })
-export class MySkills {}
+export class MySkills {
+  mySkills:Skills[]=[
+    {
+      img:'',
+      name:''
+    },
+  ]
+}
