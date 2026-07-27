ada27c912ff40696a97aea28fac951c6c6156db9
Bjoern <onoofelogmail.com>
2026-07-27 16:13:07 +0200

    created "recommendation" component

---
+++ src/app/component/recommendation/recommendation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendation',
+  imports: [],
+  templateUrl: './recommendation.html',
+  styleUrl: './recommendation.scss',
+})
+export class Recommendation {}
