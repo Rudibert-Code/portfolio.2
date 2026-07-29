29e421cd23a8ba3f271d98c54ee46c6bdadc288d
Bjoern <onoofelogmail.com>
2026-07-29 11:38:34 +0200

    - created function to mark incomplete input areas - refactoring

--- src/app/component/contact-me/contact-me.ts
+++ src/app/component/contact-me/contact-me.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
-let inputComplete:boolean = false;
+interface User{
+  name:string,
+  email:string,
+  message:string,
+  checkbox:boolean
+}
 
 @Component({
   selector: 'app-contact-me',
@@ -9,75 +14,115 @@ let inputComplete:boolean = false;
   styleUrl: './contact-me.scss',
 })
 export class ContactMe {
-  checkbox(state:string){
-    let checkbox = document.getElementById('checkbox') as HTMLDivElement;
-    checkbox.classList.toggle("checkbox");
-    checkbox.classList.remove("error");
-    checkbox.classList.toggle(state);
-  }
+  newUser:User[]=[
+    {
+      name:"",
+      email:"",
+      message:"",
+      checkbox:false
+    }
+  ]
 
   sendEmail(){
-    this.checkForm();
+    if (this.newUser[0].name && this.newUser[0].email && this.newUser[0].message && this.newUser[0].checkbox) {
+      console.log(this.newUser[0].name, this.newUser[0].email, this.newUser[0].message)
+    } else{
+      this.markIncompletion();
+    }
   }
 
-  checkForm(){
+  markIncompletion(){
     let userName = document.getElementById('input-name') as HTMLInputElement;
     let userEmail = document.getElementById('input-email') as HTMLInputElement;
     let userMessage = document.getElementById('input-text') as HTMLTextAreaElement;
     let checkboxState = document.getElementById('checkbox') as HTMLDivElement;
-    let sendButton = document.getElementById('send-button') as HTMLAnchorElement;
-    this.isNameEntered(userName);
-    this.isEmailEntered(userEmail);
-    this.isMessageEntered(userMessage);
-    this.isCheckbox(checkboxState);
 
-    if (this.isNameEntered(userName) && this.isEmailEntered(userEmail) && this.isMessageEntered(userMessage) && this.isCheckbox(checkboxState)) {
+    if (this.newUser[0].name == "") {
+      userName.classList.add("input_error");
+      userName.value="     Your name is required";  
+    }
+    if (this.newUser[0].email == "") {
+      userEmail.classList.add("input_error");
+      userEmail.value="     Your email is required";  
+    }
+    if (this.newUser[0].message == "") {
+      userMessage.classList.add("input_error");
+      userMessage.value="     Your message is required";  
+    }
+    if (this.newUser[0].checkbox == false) {
+      checkboxState.classList.add("error");  
+    }
+  }
+
+  checkFormCompletion(){
+    let sendButton = document.getElementById('send-button') as HTMLAnchorElement;
+
+    if (this.newUser[0].name && this.newUser[0].email && this.newUser[0].message && this.newUser[0].checkbox) {
+      sendButton.classList.remove("button-base-blocked");
       sendButton.classList.add("button-base");
-      inputComplete = true;
+    } else{
+      sendButton.classList.add("button-base-blocked");
+      sendButton.classList.remove("button-base");
     }
   }
 
   isNameEntered(userName:HTMLInputElement){
     if (userName.value.length >= 3 && !userName.value.includes("     Your name") && !userName.value.includes("     Your name is required")) {
-      return true;
+      this.newUser[0].name = userName.value;
     } else{
-      userName.classList.add("input_error");
-      userName.value="     Your name is required";
-      return false;
+      this.newUser[0].name = "";
     }
   }
 
   isEmailEntered(userEmail:HTMLInputElement){
     if (userEmail.value.length >= 5 && userEmail.value.includes("@") && userEmail.value.includes(".")) {
-      return true;
+      this.newUser[0].email = userEmail.value;
     } else{
-      userEmail.classList.add("input_error");
-      userEmail.value="     Your email is required";
-      return false;
+      this.newUser[0].email = "";
     }
   }
 
   isMessageEntered(userMessage:HTMLTextAreaElement){
-    if (userMessage.value.length >= 3 && userMessage.value != "     Your message..." && userMessage.value != "     Your message is required") {
-      return true;
+    if (userMessage.value.length !== 0) {
+      this.newUser[0].message = userMessage.value;
     } else{
-      userMessage.classList.add("input_error");
-      userMessage.value="     Your message is required";
-      return false;
+      this.newUser[0].message = "";
     }
   }
 
-  isCheckbox(check:HTMLDivElement){
-    if (check.classList.contains("clicked")) {
-      return true;
-    } else{
-      check.classList.add("error")
-      return false;
+  checkInputState(inputArea:string){
+    let targetInputArea = document.getElementById(inputArea) as HTMLElement;
+    targetInputArea.classList.remove("input_error");
+
+    switch (inputArea) {
+      case 'input-name':
+        this.isNameEntered(document.getElementById(inputArea) as HTMLInputElement);
+        break;
+      case 'input-email':
+        this.isEmailEntered(document.getElementById(inputArea) as HTMLInputElement);
+        break;
+      case 'input-text':
+        this.isMessageEntered(document.getElementById(inputArea) as HTMLTextAreaElement);
+        break;
+      default:
+        break;
     }
+
+    this.checkFormCompletion();
   }
 
-  resetInputState(inputArea:string){
-    let resetInputArea = document.getElementById(inputArea) as HTMLElement;
-    resetInputArea.classList.remove("input_error");
+  checkbox(state:string){
+    let checkbox = document.getElementById('checkbox') as HTMLDivElement;
+    checkbox.classList.toggle("checkbox");
+    checkbox.classList.remove("error");
+    checkbox.classList.toggle(state);
+
+    if (this.newUser[0].checkbox) {
+      this.newUser[0].checkbox = false;
+    } else{
+      this.newUser[0].checkbox = true;
+    }
+
+    this.checkFormCompletion()
   }
 }
