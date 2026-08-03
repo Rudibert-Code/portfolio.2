f8ea4a223ca55d83fefb57c34d6813a993e0e9be
Bjoern <onoofelogmail.com>
2026-08-03 19:43:38 +0200

    added legal notice text to languages.json

--- src/app/component/legal-notice/legal-notice.ts
+++ src/app/component/legal-notice/legal-notice.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, HostListener } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { LanguagePopup } from "../../component/language-popup/language-popup";
+import Language from "../../shared/language/languages.json";
 
 @Component({
   selector: 'app-legal-notice',
@@ -10,4 +11,34 @@ import { LanguagePopup } from "../../component/language-popup/language-popup";
 })
 export class LegalNotice {
   popup = inject(LanguagePopup);
+
+  languageCache = Language.DE;
+
+  @HostListener('document:click', ['$event'])
+    closeMenu(event: MouseEvent){
+      this.ngOnInit();
+    }
+
+  ngOnInit(){
+    let selectedLanguageID = String(localStorage.getItem("language"));
+    this.changeLanguage(selectedLanguageID);
+    let languageString = localStorage.getItem("test") ?? '{}';
+    console.table(JSON.parse(languageString))
+  }
+
+  changeLanguage(languageID:string){
+    switch (languageID) {
+      case 'DE':
+        this.languageCache = Language.DE;
+        break;
+
+      case 'EN':
+        this.languageCache = Language.EN;
+        break;
+      
+      default:
+        this.languageCache = Language.EN;
+        break;
+    }
+  }
 }
